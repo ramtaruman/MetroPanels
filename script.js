ad73c758ee3e16cc1c851ad19b86211f340e73dc
ramtaruman <sengupta.prakhar@gmail.com>
2022-03-18 12:52:53 +0530

    dark mode responsive dark mode button
    The dark mode button now responds to the dark mode being changed and changes its own color.

--- script.js
+++ script.js
@@ -17,9 +17,13 @@ function remove_active(){
 const checkbox = document.getElementById('checkbox');
 const nextdark = document.getElementById('textdark');
 const xicon=document.getElementById('nexticon');
+const label=document.getElementById('label');
+const ball = document.getElementById('ball');
 
 checkbox.addEventListener('change',()=>{
     document.body.classList.toggle('dark');
     nextdark.classList.toggle('dark');
     xicon.classList.toggle('xray');
+    label.classList.toggle('white');
+    ball.classList.toggle('black');
 })
